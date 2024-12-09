2dc5a5116f23600d4f10825b551db35f0e08ab4c
Arihant Gupta <72906949+GuptajiRocks@users.noreply.github.com>
2024-12-09 21:47:52 +0530

    one and two added

---
+++ 09dec/one.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void j1() {
+    char symbol[3] = {'a','b','c'};
+    for (int index = 0; index < 3; index++) {
+        cout << symbol[index];
+    }
+}
+
+int main() {
+    j1();
+    return 0;
+}
